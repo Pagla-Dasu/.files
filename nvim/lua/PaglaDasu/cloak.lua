254894d8bbc84247f263bdc4d6563226b6604be8
PaglaDasu <snehasishbabin2001@gmail.com>
2024-03-29 14:48:49 +0530

    Updates

--- nvim/lua/PaglaDasu/cloak.lua
+++ nvim/lua/PaglaDasu/cloak.lua
@@ -2,7 +2,6 @@ return {
 	"laytan/cloak.nvim",
 	config = function()
 		require("cloak").setup({
-			enabled = true,
 			cloak_character = "*",
 			-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
 			highlight_group = "Comment",
